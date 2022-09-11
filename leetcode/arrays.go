28ec85709d098357216295dcdb02a347c9997382
Mstuart712 <stuartconcept@gmail.com>
2022-09-11 17:55:06 -0600

    leet code best time to sell stock two

--- leetcode/arrays.go
+++ leetcode/arrays.go
@@ -7,8 +7,8 @@ import (
 )
 
 func StartProblem() {
-	arr2 := []int{98, 99, 99, 100}
-	fmt.Println(removeDuplicates(arr2))
+	arr2 := []int{7,1,5,3,6,4}
+	fmt.Println(maxProfit(arr2))
 }
 
 //remove duplicates of sorted int array
@@ -23,4 +23,20 @@ func removeDuplicates(nums []int) int {
     }
     sort.Ints(nums)
     return len(nums) - answer
+}
+
+func maxProfit(prices []int) int {
+	a := 0
+    for i := 0; i < len(prices); i++ {
+		if i + 1 < len(prices) && prices[i] < prices[i + 1] {
+			for j := i; j < len(prices); j++ {
+				if prices[j + 1] <  prices[j] {
+					a = a + (prices[j] - prices[i])
+					i = j
+					break
+				} 	
+			}
+		}
+	}
+	return a
 }
